5645300d48f78d1c3d5ebd5b6a9a01ef72af0e8c
Minh Tran <trcongminh.261002@gmail.com>
2025-10-14 09:56:10 +0700

    Add user authentication models and update package dependencies for better-auth

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '@/features/auth/components/login-form'
+import React from 'react'
+
+const Page = () => {
+    return (
+        <>
+            <LoginForm />
+        </>
+    )
+}
+
+export default Page
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import SignUpForm from '@/features/auth/components/signup-form'
+import React from 'react'
+
+const Page = () => {
+    return (
+        <>
+            <SignUpForm />
+        </>
+    )
+}
+
+export default Page
